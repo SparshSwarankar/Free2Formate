9cf300b292213a32c37cf3de6ffdde1f83387eeb
Sparshswarankar <sparsh0308@gmail.com>
2025-05-21 21:35:11 +0530

    railway backned url change

--- static/script.js
+++ static/script.js
@@ -75,7 +75,7 @@ document.addEventListener('DOMContentLoaded', function () {
     // const API_BASE = "http://localhost:5000/api";
     const API_BASE = window.location.hostname === "localhost"
         ? "http://localhost:5000/api"
-        : "https://free2formate.onrender.com/api";
+        : "https://free2formate-production-0000.up.railway.app/api";
 
 
 
